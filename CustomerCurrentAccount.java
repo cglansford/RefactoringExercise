cf5c921fc424588e9fc04edc239b3f6a79e02726
cglansford <58346832+cglansford@users.noreply.github.com>
2022-03-06 20:30:41 +0000

    Update CustomerCurrentAccount.java
    Created atmLogin within Customer Current Account

--- CustomerCurrentAccount.java
+++ CustomerCurrentAccount.java
@@ -17,6 +17,46 @@ public CustomerCurrentAccount(ATMCard atm, String number, double balance, ArrayL
 	this.atm = atm;
 }
 
+public boolean atmLogin(Component f) {
+	
+	int pinAttempts = 3;
+	boolean correctPin = true;
+	boolean loop = true;
+
+	while (loop) {
+		
+		if (pinAttempts == 0) {
+			JOptionPane.showMessageDialog(f,
+					"Pin entered incorrectly 3 times. ATM card locked.", "Pin",
+					JOptionPane.INFORMATION_MESSAGE);
+			atm.setValid(false);
+			
+			loop = false;
+			return false;
+		}
+
+		String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
+		int i = Integer.parseInt(Pin);
+
+		if (correctPin) {
+			if (atm.getPin() == i) {
+				loop = false;
+				JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin",
+						JOptionPane.INFORMATION_MESSAGE);
+
+			} else {
+				pinAttempts--;
+				JOptionPane.showMessageDialog(f,
+						"Incorrect pin. " + pinAttempts + " attempts remaining. pin is" + atm.getPin() , "Pin",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+
+		}
+	}
+	
+	return true;
+}
+
 public ATMCard getAtm()
 {
 	return this.atm;
